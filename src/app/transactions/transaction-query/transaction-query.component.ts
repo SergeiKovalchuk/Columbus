5d9a0d5e8176f30f670a10ed758ef0aba360c0d4
TBSergio <TBSergio@walla.com>
2019-07-16 20:04:26 +0300

    implemented dependent-select.

--- src/app/transactions/transaction-query/transaction-query.component.ts
+++ src/app/transactions/transaction-query/transaction-query.component.ts
@@ -15,18 +15,27 @@ export class TransactionQueryComponent implements OnInit, OnDestroy {
 
   constructor(private transactionQueryService: TransactionQueryService) { }
   generalServices = [];
-  providers: TransactionQuery[];
-  consumers: TransactionQuery[];
+  selectorText = new TransactionQuery('Select a Service', 'Select an Operation', 'Select an Action');
+
+  providers: TransactionQuery[] = [];
+  consumers: TransactionQuery[] = [];
+
+  providersOperations: TransactionQuery[] = [];
+  providersActions: TransactionQuery[] = [];
+
+  consumersOperations: TransactionQuery[] = [];
+  consumersActions: TransactionQuery[] = [];
 
   ngOnInit() {
+    // init form
     this.queryForm = new FormGroup({
-      generalService: new FormControl(''),
-      providerService: new FormControl(''),
-      providerOperation: new FormControl(''),
-      providerAction: new FormControl(''),
-      consumerService: new FormControl(''),
-      consumerOperation: new FormControl(''),
-      consumerAction: new FormControl('')
+      generalServiceFG: new FormControl(''),
+      providerFG: new FormGroup( {providerService: new FormControl(''),
+                                providerOperation: new FormControl(''),
+                                providerAction: new FormControl('')}),
+      consumerFG: new FormGroup(  {consumerService: new FormControl(''),
+                                consumerOperation: new FormControl(''),
+                                consumerAction: new FormControl('')})
     });
 
     this.transactionQueryService.getGeneralServices().subscribe((generalServicesArr: any[]) => {
@@ -44,8 +53,37 @@ export class TransactionQueryComponent implements OnInit, OnDestroy {
 
   onSubmit(form: NgForm) {
     console.log(this.queryForm.value);
+    console.log(this.queryForm);
   }
 
+  onProvidersServiceChange(data: TransactionQuery) {
+    this.providersOperations = this.providers.filter(prov => prov.Service === data.Service);
+    // reset sub-selects
+    // tslint:disable-next-line:no-string-literal
+    this.queryForm.controls.providerFG['controls'].providerOperation.setValue(this.selectorText.Operation, {onlySelf: true});
+    // tslint:disable-next-line:no-string-literal
+    this.queryForm.controls.providerFG['controls'].providerAction.setValue(this.selectorText.Action, {onlySelf: true});
+  }
+  onProvidersOperationChange(data: TransactionQuery) {
+    this.providersActions = this.providersOperations.filter(prov => prov.Operation === data.Operation);
+    // reset sub-selects
+    // tslint:disable-next-line:no-string-literal
+    this.queryForm.controls.providerFG['controls'].providerAction.setValue(this.selectorText.Action, {onlySelf: true});
+  }
+  onConsumersServiceChange(data: TransactionQuery) {
+    this.consumersOperations = this.consumers.filter(cons => cons.Service === data.Service);
+    // reset sub-selects
+    // tslint:disable-next-line:no-string-literal
+    this.queryForm.controls.consumerFG['controls'].consumerOperation.setValue(this.selectorText.Operation, {onlySelf: true});
+    // tslint:disable-next-line:no-string-literal
+    this.queryForm.controls.consumerFG['controls'].consumerAction.setValue(this.selectorText.Action, {onlySelf: true});
+  }
+  onConsumersOperationChange(data: TransactionQuery) {
+    this.consumersActions = this.consumers.filter(cons => cons.Operation === data.Operation);
+    // reset sub-selects
+    // tslint:disable-next-line:no-string-literal
+    this.queryForm.controls.consumerFG['controls'].consumerAction.setValue(this.selectorText.Action, {onlySelf: true});
+  }
   ngOnDestroy(): void {
   }
 }
